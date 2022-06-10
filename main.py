060308b2af54d9def040c8221e83dff421aabe6e
DavidZahour <102311393+DavidZahour@users.noreply.github.com>
2022-06-10 18:39:07 +0200

    fix of dir

--- main.py
+++ main.py
@@ -1,30 +1,88 @@
-import csv
+import json
+import warnings
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+from API_KEY import token
+import datetime
 
-print("Hello world from python")
+transport = AIOHTTPTransport(url="https://hannah.wpj.cloud/admin/graphql/", headers={'X-Access-Token': token})
+client = Client(transport=transport, fetch_schema_from_transport=True)
 
-csvlt = '\n'
-csvdel = ','
-csvquo = '"'
-with open('/data/in/tables/source.csv', mode='rt', encoding='utf-8') as in_file, \
-        open('/data/out/tables/odd.csv', mode='wt', encoding='utf-8') as odd_file, \
-        open('/data/out/tables/even.csv', mode='wt', encoding='utf-8') as even_file:
-    lazy_lines = (line.replace('\0', '') for line in in_file)
-    reader = csv.DictReader(lazy_lines, lineterminator=csvlt, delimiter=csvdel,
-                            quotechar=csvquo)
 
-    even_writer = csv.DictWriter(odd_file, fieldnames=reader.fieldnames,
-                                 lineterminator=csvlt, delimiter=csvdel,
-                                 quotechar=csvquo)
-    even_writer.writeheader()
+def get_product(id):
+    """
+    Return products of given ID
+    :param id: Id of products
+    :type id: int
+    :return: products details
+    :rtype: Dict
+    """
+    # Todo implement more stuff from https://graphql-docs.wpjshop.cz/
+    query = gql(
+        """{{
+        product(id:{}){{
+        id
+        variationId
+        url
+        code
+        ean
+        inStore
+        title
+        variationTitle
+        description
+        }}
+        }}""".format(id)
+    )
+    result = client.execute(query)
+    return result['product']
 
-    odd_writer = csv.DictWriter(even_file, fieldnames=reader.fieldnames,
-                                lineterminator=csvlt, delimiter=csvdel,
-                                quotechar=csvquo)
-    odd_writer.writeheader()
-    i = 0
-    for row in reader:
-        if i % 2 == 0:
-            even_writer.writerow(row)
-        else:
-            odd_writer.writerow(row)
-        i = i + 1
+
+def get_orders(num_of_days=7):
+    """
+    This function load oders from x last x days TODO implement next day
+    :param num_of_days: Num of days in history to load
+    :type num_of_days: int
+    :return: List of history oders
+    :rtype: Dict
+    """
+    date = datetime.datetime.now() - datetime.timedelta(days=num_of_days)
+    date = date.isoformat()
+    query = gql(
+        """
+        {{
+        orders(limit: 10000, sort: {{ dateCreated: DESC }}, filter: {{ dateFrom:"{}"}}) {{
+        hasNextPage
+        hasPreviousPage
+        items {{
+        id
+        code
+        dateCreated
+        totalPrice {{
+            withVat
+            withoutVat
+        }}
+        currency {{
+            code
+            name
+        }}
+        }}
+        }}
+    }}
+    """.format(date)
+    )
+    result = client.execute(query)
+
+    if (result["orders"]["hasNextPage"] or result["orders"]["hasPreviousPage"]):
+        warnings.warn("Next_page not implemented", Warning)
+
+    return result["orders"]["items"]
+
+
+if __name__ == '__main__':
+    orders = get_orders()
+
+    product_lst = []
+    for i in orders:
+        product_lst.append(get_product(i['id']))
+    print(product_lst)
+    print(orders)
